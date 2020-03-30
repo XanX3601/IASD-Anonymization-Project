e364141a3fb985881a3a6e10e51007cb6ee3482f
Elie KADOCHE <eliekadoche78@gmail.com>
2020-03-30 17:11:45 +0200

    Corrected input size vector

--- script.py
+++ script.py
@@ -10,6 +10,6 @@ for i in range(1, 11):
     print("Training classifier {}".format(i))
     subprocess.run(["python", "./train_net.py", "--path", "./networks/classifiers/classifier_{}.pt".format(i), "--dataset", "{}".format(i)])
 subprocess.run(["python", "./extract_weights.py", "--dir", "./networks/classifiers","--out", "./meta_datasets"])
-subprocess.run(["python", "./create_meta_net.py", "--path", "./networks/meta_classifier.pt", "--input-size-vector", "1600"])
+subprocess.run(["python", "./create_meta_net.py", "--path", "./networks/meta_classifier.pt", "--input-size-vector", "3200"])
 subprocess.run(["python", "./train_meta_net.py", "--path", "./networks/meta_classifier.pt", "--dataset", "./meta_datasets"])
 subprocess.run(["python", "./bikes_or_not_bikes_that_is_the_question.py", "--target", "./networks/target_classifier.pt", "--meta", "./networks/meta_classifier.pt"])
