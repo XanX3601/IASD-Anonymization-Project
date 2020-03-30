1d2d31e07f9e2cbec56a82ae23e83513bba23410
Thomas Petiteau <thomas.petiteau@outlook.com>
2020-03-30 22:25:36 +0200

    updated script stats

--- script_stats.py
+++ script_stats.py
@@ -1,28 +1,32 @@
 import subprocess
 import os
 
+epoch = 1
+
 bikes = 0
 no_bikes = 0
 
 FNULL = open(os.devnull, 'w')
 
 while True:
-    subprocess.run(["python", "./download_data.py"])
+    subprocess.run(["python", "./download_data.py"], stdout=FNULL, stderr=FNULL)
     subprocess.run(["mkdir", "./networks", "./networks/classifiers", "./meta_datasets"], stdout=FNULL, stderr=FNULL)
-    subprocess.run(["python", "./create_net.py", "--path", "./networks/target_classifier.pt"], stdout=FNULL, stderr=FNULL)
+    subprocess.run(["python", "./create_net.py", "--path", "./networks/target_classifier.pt", "--cuda"], stdout=FNULL, stderr=FNULL)
     for i in range(1, 11):
-        subprocess.run(["python", "./create_net.py", "--path", "./networks/classifiers/classifier_{}.pt".format(i)], stdout=FNULL, stderr=FNULL)
-    subprocess.run(["python", "./train_net.py", "--path", "./networks/target_classifier.pt", "--dataset", "0"], stdout=FNULL, stderr=FNULL)
+        subprocess.run(["python", "./create_net.py", "--path", "./networks/classifiers/classifier_{}.pt".format(i), "--cuda"], stdout=FNULL, stderr=FNULL)
+    subprocess.run(["python", "./train_net.py", "--path", "./networks/target_classifier.pt", "--dataset", "0", "--epochs", str(epoch), "--cuda"], stdout=FNULL, stderr=FNULL)
     for i in range(1, 11):
-        subprocess.run(["python", "./train_net.py", "--path", "./networks/classifiers/classifier_{}.pt".format(i), "--dataset", "{}".format(i)], stdout=FNULL, stderr=FNULL)
-    subprocess.run(["python", "./extract_weights.py", "--dir", "./networks/classifiers","--out", "./meta_datasets"], stdout=FNULL, stderr=FNULL)
-    subprocess.run(["python", "./create_meta_net.py", "--path", "./networks/meta_classifier.pt", "--input-size-vector", "3200"], stdout=FNULL, stderr=FNULL)
-    subprocess.run(["python", "./train_meta_net.py", "--path", "./networks/meta_classifier.pt", "--dataset", "./meta_datasets"], stdout=FNULL, stderr=FNULL)
-    completed_process = subprocess.run(["python", "./bikes_or_not_bikes_that_is_the_question.py", "--target", "./networks/target_classifier.pt", "--meta", "./networks/meta_classifier.pt"], stdout=subprocess.PIPE)
+        subprocess.run(["python", "./train_net.py", "--path", "./networks/classifiers/classifier_{}.pt".format(i), "--dataset", "{}".format(i), "--epochs", str(epoch), "--cuda"], stdout=FNULL, stderr=FNULL)
+    subprocess.run(["python", "./extract_weights.py", "--dir", "./networks/classifiers","--out", "./meta_datasets", "--cuda"], stdout=FNULL, stderr=FNULL)
+    subprocess.run(["python", "./create_meta_net.py", "--path", "./networks/meta_classifier.pt", "--input-size-vector", "3200", "--cuda"], stdout=FNULL, stderr=FNULL)
+    subprocess.run(["python", "./train_meta_net.py", "--path", "./networks/meta_classifier.pt", "--dataset", "./meta_datasets", "--cuda"], stdout=FNULL, stderr=FNULL)
+    completed_process = subprocess.run(["python", "./bikes_or_not_bikes_that_is_the_question.py", "--target", "./networks/target_classifier.pt", "--meta", "./networks/meta_classifier.pt", "--cuda"], stdout=subprocess.PIPE)
 
-    answer = int(str(completed_process.stdout)[2])
+    answer = 'no bikes' in str(completed_process.stdout)
 
-    if answer == 0:
+    print(answer)
+
+    if answer:
         no_bikes += 1
     else:
         bikes += 1
